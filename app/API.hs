af83d30e1e75ecad3bf7d418fcee16488dadb2b7
Jason Feingold <jfeingold@gmail.com>
2026-04-24 13:25:17 -0400

    listWidgets_

--- app/API.hs
+++ app/API.hs
@@ -37,8 +37,7 @@ withErrorHandling (API m) = API $ ReaderT $ \pool ->
 listWidgets :: API [Widget]
 listWidgets = withErrorHandling $ API $ do
     pool <- ask
-    liftIO $ withResource pool $ \conn -> do
-        query_ conn "SELECT id, name, created_at, deleted_at FROM web_hs.widgets" :: IO [Widget]
+    liftIO $ withResource pool listWidgets_
 
 createWidget :: WidgetWip -> API Widget
 createWidget wip = withErrorHandling $ API $ do
@@ -47,3 +46,7 @@ createWidget wip = withErrorHandling $ API $ do
     liftIO $ withResource pool $ \conn -> do
         _ <- execute conn "INSERT INTO web_hs.widgets (id, name, created_at, deleted_at) VALUES (?, ?, ?, ?)" widget
         return widget
+
+listWidgets_ :: Connection -> IO [Widget]
+listWidgets_ conn =
+    query_ conn "SELECT id, name, created_at, deleted_at FROM web_hs.widgets" :: IO [Widget]
