45c375a59508530dda84f4897d59299c68218d87
Jason Feingold <jfeingold@gmail.com>
2026-04-24 18:19:19 -0400

    client hello

--- app/Client.hs
+++ app/Client.hs
@@ -10,21 +10,30 @@ import Util
 
 main :: IO ()
 main = do
+    helloRequest
     args <- getArgs
     if null args
         then putStrLn "No widgets specified."
         else do
             mapM_ createWidgetRequest args
-    
     putStrLn "Widgets!"
     listWidgetsRequest
+
+helloRequest :: IO ()
+helloRequest = do
+    request <- parseRequest "GET http://localhost:3000/"
+    response <- httpLBS request
+    let body = getResponseBody response
+    writeLine ["RESPONSE: ", show body]
+    return ()
 
 createWidgetRequest :: String -> IO ()
 createWidgetRequest name = do
     let body = CreateWidget (pack name)
     request <- parseRequest "PUT http://localhost:3000/widgets"
     let request' = setRequestBodyJSON body request
-    _ <- httpJSON request' :: IO (Response Widget)
+    widget <- httpJSON request' :: IO (Response Widget)
+    writeLine  ["Created: ", show widget]
     return ()
 
 listWidgetsRequest :: IO ()
@@ -32,4 +41,4 @@ listWidgetsRequest = do
     request <- parseRequest "GET http://localhost:3000/widgets"
     response <- httpJSON request :: IO (Response [Widget])
     let widgets = getResponseBody response
-    sequence_ $ fmap (\(i, w) -> writeLine [show i, " ", show w]) (zip [(1 :: Int)..] widgets)
+    sequence_ $ fmap (\(i, w) -> writeLine [show i, " ", show w]) $ zip [(1 :: Int)..] widgets
